5f8e7458dfac7200bdf1ff3073da29a44a11d879
Ryan S <ryansouza@usp.br>
2023-07-19 14:45:08 -0300

    CI/CD

---
+++ test/video-service_test.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/entity"
+	videoService "gitlab.com/pragmaticreviews/golang-gin-poc/service"
+)
+
+const (
+	TITLE       = "Video Title"
+	DESCRIPTION = "Video Description"
+	URL         = "https://youtu.be/JgW-i2QjgHQ"
+)
+
+func getVideo() entity.Video {
+	return entity.Video{
+		Title:       TITLE,
+		Description: DESCRIPTION,
+		URL:         URL,
+	}
+}
+
+func TestFindAll(t *testing.T) {
+	service := videoService.New()
+
+	service.Save(getVideo())
+
+	videos := service.FindAll()
+
+	firstVideo := videos[0]
+	assert.NotNil(t, videos)
+	assert.Equal(t, TITLE, firstVideo.Title)
+	assert.Equal(t, DESCRIPTION, firstVideo.Description)
+	assert.Equal(t, URL, firstVideo.URL)
+}
